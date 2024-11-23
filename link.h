74d48bab49fc3eb298461dd60c5216dff8ff80f1
Haadi Khan <haadi.khan7191@gmail.com>
2024-11-23 17:52:41 -0500

    Updated Link to include additional fields

--- link.h
+++ link.h
@@ -9,6 +9,8 @@ class Link {
     int owner; // uses players index in GameController
     bool isVisible;
     bool isDead;
+    bool isTrojan;
+    bool isAndOne;
     
     public:
         Link(char name, char type, int strength, int owner, int movement = 1, bool isVisible = true, bool isDead = false);
