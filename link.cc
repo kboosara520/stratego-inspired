73f4e07a0a9c9b63e98c69fde506bdd06f23a79a
Haadi Khan <haadi.khan7191@gmail.com>
2024-11-24 17:44:37 -0500

    Link implementation getters and setters

---
+++ link.cc
@@ -0,0 +1,35 @@
+#include "link.h"
+Link::Link(char name, char type, int strength, int owner, int movement, bool isVisible, bool isDead, bool isTrojan, bool isAndOne):name{name}, type{type}, strength{strength}, owner{owner}, movement{movement}, isVisible{isVisible}, isDead{isDead}, isTrojan{isTrojan}, isAndOne{isAndOne}{}
+char Link::getName() {
+    return name;
+}
+char Link::getType() {
+    return type;
+}
+int Link::getOwner() {
+    return owner;
+}
+int Link::getMovement() {
+    return movement;
+}
+int Link::getStrength() {
+    return strength;
+}
+bool Link::getIsTrojan() {
+    return isTrojan;
+}
+bool Link::getIsDead() {
+    return isDead;
+}
+void Link::setIsVisble(bool isVisible) {
+    this->isVisible = isVisible;
+}
+void Link::setIsDead(bool isDead) {
+    this->isDead = isDead;
+}
+void Link::setMovement(int movement) {
+    this->movement = movement;
+}
+void Link::setIsTrojan(bool isTrojan) {
+    this->isTrojan = isTrojan;
+}
