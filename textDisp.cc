c2bc2e02739b40bdc3382a3801ddbd32e96336f1
Haadi Khan <haadi.khan7191@gmail.com>
2024-11-26 17:17:45 -0500

    textdisplay update

--- textDisp.cc
+++ textDisp.cc
@@ -1,20 +1,32 @@
 #include "textDisp.h"
+using namespace std;
 
 TextDisp::TextDisp(Board * board, std::vector<Player *> players): board{board}, players{players}{}
 void TextDisp::displayBoard() {
     
-    // need to get a map of the links from the player.
+    // MAY NEED TO CHECK IF ITS A TROJAN, HAVE A DIFFERENT OUTPUT
+
+    // have a method that returns a reference to the links owned by the player
+    auto &p1Links = players[0]->getLinks(); //std::map<char,std::unique_ptr<Link>> links;
+    auto &p2Links = players[1]->getLinks();
     cout << "Player 1" << endl;
     cout << "Downloaded: " << players[0]->getData() << "D, " << players[0]->getVirus() <<"V" << endl;
     cout << "Abilities: " << players[0]->getAbilityCount() << endl;
 
-    // handle printing individual pieces here
-    // have a range based for loop
     for (int i = 0; i < BOARDSIZE/2; i++) {
         for (int j = 0; j < BOARDSIZE/2; j++) {
+            
             // if its player 1's turn or the piece is visible, display piece
             // otherwise show as question mark
+            char index = 'A' + i + j;
+            cout << p1Links[index]->getName() << " ";
+            if (ticks % 2 == 0 || p1Links[index]->isVisble()) {
+                cout << p1Links[index]->getType() << p1Links[index]->getStrength() << " ";
+            } else {
+                cout << "?  ";
+            }
         }
+        cout << endl;
     }
 
     cout << "========" << endl;
@@ -29,12 +41,17 @@ void TextDisp::displayBoard() {
     cout << "Downloaded: " << players[1]->getData() << "D, " << players[1]->getVirus() <<"V" << endl;
     cout << "Abilities: " << players[1]->getAbilityCount() << endl;
 
-    // handle printing individual pieces here
-    // have a range based for loop
     for (int i = 0; i < BOARDSIZE/2; i++) {
         for (int j = 0; j < BOARDSIZE/2; j++) {
-            // if its player 2's turn or the piece is visible, display piece
-            // otherwise show as question mark
+            char index = 'a' + i + j;
+            cout << p1Links[index]->getName() << " ";
+            if (ticks % 2 == 1 || p2Links[index]->isVisble()) {
+                cout << p2Links[index]->getType() << p2Links[index]->getStrength() << " ";
+            } else {
+                cout << "?  ";
+            }
         }
+        cout << endl;
     }
+    ticks += 1;
 }
